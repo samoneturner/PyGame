d699a5da0a54478665e98ea26f75c341f1b54418
Samone Turner <Samoneturner_Turner1@baylor.edu>
2023-02-21 17:08:24 -0600

    limiting the ships range

--- ship.py
+++ ship.py
@@ -1,36 +1,37 @@
-import pygame 
+import pygame
+
 
 class Ship:
     """A class to mamage the ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
-        self.screen = ai_game.screen 
-        self.settings = ai_game.settings 
-        self.screen_rect = ai_game.screen.get_rect() 
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
 
-        #Load the ship image and get its rect. 
-        self.image = pygame.image.load('images/ship.bmp') 
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load("ship.bmp")
         self.rect = self.image.get_rect()
 
-        #Start each new ship at the bottom center of the screen. 
-        self.rect.midbottom = self.screen_rect.midbottom 
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
 
-        #Store a decimal value for the ship's horizontal position. 
+        # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
 
-        #Movement flag 
-        self.moving_right = False 
+        # Movement flag
+        self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
         # Update the ship's x value, not the rect.
-        if self.moving_right:
-            self.x += self.settings.ship_speed 
-        if self.moving_left:
-            self.x -= self.settings.ship_speed 
-        # Update rect object from self.x 
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        # Update rect object from self.x
         self.rect.x = self.x
 
     def blitme(self):
