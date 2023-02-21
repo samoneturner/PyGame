ab30238ee823fd5317751d28f6c0486afd27f259
Samone Turner <Samoneturner_Turner1@baylor.edu>
2023-02-21 17:47:25 -0600

    deleting old bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,6 +29,12 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            # Get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                self.bullets.remove(bullet)
+            print(len(self.bullets))
+
             self._update_screen()
 
     def _check_events(self):
