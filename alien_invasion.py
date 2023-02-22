c504f126d5172b77d7c0e4d63e9f3db00324fe79
Samone Turner <Samoneturner_Turner1@baylor.edu>
2023-02-22 13:42:38 -0600

    refactoring _create_fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -109,6 +109,7 @@ class AlienInvasion:
         for alien_number in range(number_aliens_x):
             # Create an alien and place it in the row.
             alien = Alien(self)
+            alien_width = alien.rect.width
             alien.x = alien_width + 2 * alien_width * alien_number
             alien.rect.x = alien.x
             self.aliens.add(alien)
