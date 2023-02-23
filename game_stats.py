c1dcd0eca11fee06571a5af95125ffd872619de0
Samone Turner <Samoneturner_Turner1@baylor.edu>
2023-02-23 14:47:41 -0600

    Adding play button

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # Start game in an inactive state.
+        self.game_active = False
+
         # Start alien Invasion in an active state.
         self.game_active = True
 
